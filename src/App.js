d3088ea60185d46af04e4d7404a09d77207b576a
İsmail HASIR <ismailhasir@gmail.com>
2022-02-13 05:10:38 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import "./App.css";
+import { useState } from "react";
+import Title from "./components/Title";
+
+function App() {
+  const [showEvents, setShowEvents] = useState(true);
+
+  const [events, setEvents] = useState([
+    { title: "mario's birtday party", id: 1 },
+    { title: "bowser's live stream", id: 2 },
+    { title: "race on moo moo farm", id: 3 },
+  ]);
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id;
+      });
+    });
+    console.log(id);
+  };
+
+  return (
+    <div className="App">
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>hide events</button>
+        </div>
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>show events</button>
+        </div>
+      )}
+      {showEvents &&
+        events.map((event, index) => (
+          <div key={event.id}>
+            <h2>
+              {index} -{event.title}
+            </h2>
+            <button onClick={() => handleClick(event.id)}>delete event</button>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default App;
